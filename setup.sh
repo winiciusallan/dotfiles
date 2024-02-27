4f9084e6f6e9088d75041ecbef3db7bd38c3536e
Winicius Silva <winiciusab12@gmail.com>
2024-02-27 15:07:44 -0300

    changes in tmux/tmux-behavior.conf tmux/tmux-design.conf tmux/tmux.conf

---
+++ setup.sh
@@ -0,0 +1,42 @@
+#/bin/bash
+
+# STOW_PATH=$HOME/dotfiles/stow
+
+usage() {
+	echo "
+    usage: ./setup.sh [package] [-h, --help]
+
+    Aplica os dotfiles que estão nesse repositório. É possível aplicar todos ou apenas um dos pacotes.
+
+    options:
+        package: O pacote que as configurações irão ser aplicadas. [optional]
+        -h, --help: Mostra essa mensagem de ajuda com as opções do script. [optional]
+    "
+}
+
+if [[ "$#" -gt 1 ]]; then
+	echo "
+    This script accept only 0 or 1 argument"
+	usage
+	exit 1
+fi
+
+if [[ $1 = '-h' ]] || [[ $1 = '-help' ]]; then
+	usage
+	exit 0
+fi
+
+if [[ -z $@ ]]; then
+	for dir in $(ls -d */ | grep -v "scripts" | tr -d '/'); do
+		echo $dir
+		stow -D --target=$HOME $dir
+	done
+
+	stow --target=$HOME stow
+else
+	stow --target=$HOME $1 2>/dev/null
+
+	if [[ $? -eq 1 ]]; then
+		echo "[ERROR] The symbolic link already exists. Operation was not perfomed."
+	fi
+fi
